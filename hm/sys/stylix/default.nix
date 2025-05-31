786a581e9aaffd811ec572041f520d84aad40b94
Anton <antnemr@proton.me>
2025-05-31 13:26:09 +0200

    updated stylix

--- hm/sys/stylix/default.nix
+++ hm/sys/stylix/default.nix
@@ -2,15 +2,18 @@
 
 {
   stylix = {
-    #   base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-sulphurpool.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-sulphurpool.yaml";
   
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
   
-    image = ./lanterns_01.jpg;
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/dharmx/walls/refs/heads/main/unsorted/a_group_of_white_lanterns_with_black_writing_on_them.jpg";
+      hash = "sha256-+yUcgUKgezEL2XJ098wK1bZ+zPqvniNP4DmJ8qRV2ug=";
+    };
 
-    targets.hyprland.hyprpaper.enable = false;
-    targets.hyprlock.enable = false;
+    # targets.hyprland.hyprpaper.enable = false;
+    # targets.hyprlock.enable = false;
 
     cursor.size = 24;
 
