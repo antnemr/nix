3680c96937939c520fc894c05e5c567e5fa23b1a
Anton <antnemr@proton.me>
2025-07-22 10:23:05 +0200

    stylix

--- home/desktop/stylix/default.nix
+++ home/desktop/stylix/default.nix
@@ -20,6 +20,8 @@ in
     
     # base16Scheme = ;
 
+    targets.zen-browser.profileNames = [ "8yfxlizu.Default Profile" ];
+
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
