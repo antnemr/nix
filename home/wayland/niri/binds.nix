c00d1664daf76f99c2eba95c9e3b7543e2a01f00
Anton <antnemr@proton.me>
2025-07-22 10:10:51 +0200

    niri

--- home/wayland/niri/binds.nix
+++ home/wayland/niri/binds.nix
@@ -39,28 +39,19 @@
     "Mod+End".action = focus-column-last;
 
     # Switch Workspace
-    # "Mod+1".action = focus-workspace 1;
-    # "Mod+2".action = focus-workspace 2;
-    # "Mod+3".action = focus-workspace 3;
-    # "Mod+4".action = focus-workspace 4;
-    # "Mod+5".action = focus-workspace 5;
-    # "Mod+6".action = focus-workspace 6;
-    # "Mod+7".action = focus-workspace 7;
-    # "Mod+8".action = focus-workspace 8;
-    # "Mod+9".action = focus-workspace 9;
+    "Mod+1".action = focus-workspace 1;
+    "Mod+2".action = focus-workspace 2;
+    "Mod+3".action = focus-workspace 3;
+    "Mod+4".action = focus-workspace 4;
+    "Mod+5".action = focus-workspace 5;
+    "Mod+6".action = focus-workspace 6;
+    "Mod+7".action = focus-workspace 7;
+    "Mod+8".action = focus-workspace 8;
+    "Mod+9".action = focus-workspace 9;
     "Mod+J".action = focus-window-or-workspace-down;
     "Mod+K".action = focus-window-or-workspace-up;
 
     # Move to Workspace
-    # "Mod+Shift+1".action = move-window-to-workspace 1;
-    # "Mod+Shift+2".action = move-window-to-workspace 2;
-    # "Mod+Shift+3".action = move-window-to-workspace 3;
-    # "Mod+Shift+4".action = move-window-to-workspace 4;
-    # "Mod+Shift+5".action = move-window-to-workspace 5;
-    # "Mod+Shift+6".action = move-window-to-workspace 6;
-    # "Mod+Shift+7".action = move-window-to-workspace 7;
-    # "Mod+Shift+8".action = move-window-to-workspace 8;
-    # "Mod+Shift+9".action = move-window-to-workspace 9;
     "Mod+Shift+J".action = move-column-to-workspace-down;
     "Mod+Shift+K".action = move-column-to-workspace-up;
   };
