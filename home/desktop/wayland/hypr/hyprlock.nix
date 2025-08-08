368ff0bbe38d6ec8acc647714acdf98d73f94b86
Anton <antnemr@proton.me>
2025-08-08 13:33:31 +0200

    fastfetch

--- home/desktop/wayland/hypr/hyprlock.nix
+++ home/desktop/wayland/hypr/hyprlock.nix
@@ -1,12 +1,26 @@
 {
   programs.hyprlock = {
     enable = true;
-
     settings = {
       general = {
         disable_loading_bar = true;
         immediate_render = true;
         hide_cursor = false;
+      };
+      background = {
+        blur_passes = 3;
+        blur_size = 8;
+      };
+      animations = {
+        enable = true;
+        fade_in = {
+          duration = 300;
+          bezier = "easeOutQuint";
+        };
+        fade_out = {
+          duration = 300;
+          bezier = "easeOutQuint";
+        };
       };
     };
   };
