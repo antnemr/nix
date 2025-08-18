7ede9795266c4bb3d9cc0e2737bd7c8f54f3cc41
Anton <antnemr@proton.me>
2025-08-18 20:57:38 +0200

    stylix

--- home/desktop/stylix/default.nix
+++ home/desktop/stylix/default.nix
@@ -7,7 +7,8 @@ in
     caroline = "${scheme}/caroline.yaml";
     catpuccin = "${scheme}/catpuccin-mocha.yaml";
     everforest = "${scheme}/everforest-dark-hard.yaml";
-    material-gruvbox = "${scheme}/gruvbox-material-dark-medium.yaml";
+    material-gruvbox = "${scheme}/gruvbox-material-dark-soft.yaml";
+    material-gruvbox-light = "${scheme}/gruvbox-material-light-soft.yaml";
     kanagawa = "${scheme}/kanagawa.yaml";
     rose-pine = "${scheme}/rose-pine.yaml";
     tokyo-night = "${scheme}/tokyo-night-dark.yaml";
@@ -16,12 +17,39 @@ in
     charcoal-light = "${scheme}/charcoal-light.yaml";
   in {
   stylix = {
+    enable = true;
+    polarity = "dark";
+
     image = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/dharmx/walls/refs/heads/main/unsorted/a_group_of_white_lanterns_with_black_writing_on_them.jpg";
       hash = "sha256-+yUcgUKgezEL2XJ098wK1bZ+zPqvniNP4DmJ8qRV2ug=";
     };
     
-    base16Scheme = ./charcoal-dark.yaml;
+    base16Scheme = {
+      system = "base16";
+      name = "cozy";
+      author = "Anton (https://github.com/morhetz/gruvbox)";
+      variant = "dark";
+
+      palette = {
+        base00 = "353433";
+        base01 = "cf7e00";
+        base02 = "fdc243";
+        base03 = "ffe1ba";
+        base04 = "b8ab66";
+        base05 = "e69952";
+        base06 = "e7c66d";
+        base07 = "e8d5bf";
+        base08 = "afa090";
+        base09 = "f09300";
+        base0A = "ffd88f";
+        base0B = "fff2e4";
+        base0C = "cfc092";
+        base0D = "f5b171";
+        base0E = "fed772";
+        base0F = "ffffff";
+      };
+    };
 
     fonts = {
       monospace = {
@@ -61,7 +89,5 @@ in
       dark = "Papirus-Dark";
       light = "Papirus-Light";
     };
-    
-    enable = true;
   };
 }
