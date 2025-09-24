35c524015847940bc24c7a3a24f9047a07e4e421
Anton <antnemr@proton.me>
2025-09-24 22:57:25 +0200

    hyprland, stylix

--- home/desktop/stylix.nix
+++ home/desktop/stylix.nix
@@ -33,21 +33,21 @@ in
 
     palette = {
       base00 = "18130b";
-      base01 = "cf7e00";
-      base02 = "fdc243";
-      base03 = "ffe1ba";
-      base04 = "b8ab66";
-      base05 = "e69952";
-      base06 = "e7c66d";
-      base07 = "e8d5bf";
-      base08 = "afa090";
-      base09 = "f09300";
-      base0A = "ffd88f";
-      base0B = "fff2e4";
-      base0C = "cfc092";
-      base0D = "f5b171";
-      base0E = "fed772";
-      base0F = "ffffff";
+      base01 = "1e1812";
+      base02 = "35291d";
+      base03 = "66553f";
+      base04 = "a28662";
+      base05 = "f2be6e";
+      base06 = "d6b891";
+      base07 = "292016";
+      base08 = "887254";
+      base09 = "d6b891";
+      base0A = "c0a179";
+      base0B = "927a60";
+      base0C = "a28662";
+      base0D = "d6b891";
+      base0E = "a28662";
+      base0F = "887254";
     };
   };
 
--- home/desktop/wayland/hypr/default.nix
+++ home/desktop/wayland/hypr/default.nix
@@ -49,30 +49,41 @@
 	      gaps_in = 8;
 	      gaps_out = 20;
         layout = "dwindle";
-        "col.inactive_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base00})";
+        "col.active_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base05})";
+        "col.inactive_border" = lib.mkForce "rgb(${config.lib.stylix.colors.base03})";
       };
 
       animations = {
 	      enabled = true;
+        bezier = [
+          "linear, 0, 0, 1, 1"
+          "md3_standard, 0.2, 0, 0, 1"
+          "md3_decel, 0.05, 0.7, 0.1, 1"
+          "md3_accel, 0.3, 0, 0.8, 0.15"
+          "overshot, 0.05, 0.9, 0.1, 1.1"
+          "crazyshot, 0.1, 1.5, 0.76, 0.92 "
+          "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+          "menu_decel, 0.1, 1, 0, 1"
+          "menu_accel, 0.38, 0.04, 1, 0.07"
+          "easeInOutCirc, 0.85, 0, 0.15, 1"
+          "easeOutCirc, 0, 0.55, 0.45, 1"
+          "easeOutExpo, 0.16, 1, 0.3, 1"
+          "softAcDecel, 0.26, 0.26, 0.15, 1"
+          "md2, 0.4, 0, 0.2, 1 # use with .2s duration"
+        ];
+        animation = [
+          "windows, 1, 3, md3_decel, popin 60%"
+          "windowsIn, 1, 3, md3_decel, popin 60%"
+          "windowsOut, 1, 3, md3_accel, popin 60%"
+          "border, 1, 10, default"
+          "fade, 1, 3, md3_decel"
+          "layersIn, 1, 3, menu_decel, slide"
+          "layersOut, 1, 1.6, menu_accel"
+          "fadeLayersIn, 1, 2, menu_decel"
+          "fadeLayersOut, 1, 4.5, menu_accel"
+          "workspaces, 1, 7, menu_decel, slide"
+        ];
       };
-      
-      animation = [
-        "windows, 1, 6, wind, slide"
-        "windowsIn, 1, 6, winIn, slide"
-        "windowsOut, 1, 5, winOut, slide"
-        "windowsMove, 1, 5, wind, slide"
-        "border, 1, 10, liner"
-        "borderangle, 1, 100, linear, loop"
-        "fade, 1, 10, default"
-        "workspaces, 1, 5, wind"
-      ];
-      bezier = [
-        "wind, 0.05, 0.9, 0.1, 1.05"
-        "winIn, 0.1, 1.1, 0.1, 1.1"
-        "winOut, 0.3, -0.3, 0, 1"
-        "liner, 1, 1, 1, 1"
-        "linear, 0.0, 0.0, 1.0, 1.0"
-      ];
 
       decoration = {
         rounding = 25;
